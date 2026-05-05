f5ad9dbd05a826e004f7f52dfffeda7d6df07152
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-05 10:34:29 +0700

    Add tampil info

---
+++ Produk.java
@@ -0,0 +1,44 @@
+public class Produk {
+    private String nama;
+    String kategori;
+    private double harga;
+    private int stok;
+
+    public Produk (String nama, double harga, int stok, String kategori){
+    this.nama = nama;
+    this.kategori = kategori;
+    this.harga = harga; 
+    this.stok = stok;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public double getHarga(){
+        return harga;
+    }
+
+    public int getStok(){
+        return stok;
+    }
+
+    
+    public void setHarga(double harga){
+        this.harga = harga;
+    }
+
+    public void setStok (int stok){
+        this.stok = stok;
+    }
+
+   
+    public void tampilkanInfo(){
+        System.out.println("Produk: " + nama);
+        System.out.println("Kategori: " + kategori);
+        System.out.println("Harga: " + harga);
+        System.out.println("Stok: " + stok);
+        System.out.println("-------------");
+
+    }
+}
