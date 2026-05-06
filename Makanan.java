a827e96456c5fa8b5977cd068ede3c80b0fceda3
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-06 10:55:07 +0700

    Add inheritance

--- Makanan.java
+++ Makanan.java
@@ -1,10 +1,31 @@
-public class Makanan extends Produk {
-    private int expired;
+//- Superclass--------------------------------------
+//- Subclass 1: Elektronik----------------------------
+//- Subclass 2: Makanan------------------------------
+class Makanan extends Produk {
+    private String kadaluarsa;
+    private String kategori;
+
     public Makanan(String nama, double harga, int stok, String kategori, int expired) {
-        super(nama, harga, stok, kategori);
-        this.expired = expired;
+        super(nama, harga, stok);
+        this.kadaluarsa = kadaluarsa;
+        this.kategori = kategori;
     }
 
+    public String getKadaluarsa(){
+        return kadaluarsa;
+    }
+
+    public String getKategori(){
+        return kategori;
+    }
+
+
+
+
+
+
+
+    
     public void tampikanData(){
         super.tampilkanInfo();
         System.out.println("Masa expired: " + "Hari");
